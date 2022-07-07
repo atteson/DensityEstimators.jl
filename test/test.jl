e2384b0ce1ad814dc5918ff255b4ca981945d2ea
Kevin Atteson <kevin@atteson.com>
2022-07-07 09:56:18 -0400

    change precision

--- test/test.jl
+++ test/test.jl
@@ -18,8 +18,8 @@ p = histogram( x, normalize=:pdf, fillalpha=0.0, size=[1000,800] )
 @time bfy = pdf.( bf, r );
 plot!( p, r, bfy, label="" )
 
-fft = FFTKernelDensityEstimator( dist, h, bits=20 )
-@time fft = fit( fft, x );
+fft = FFTKernelDensityEstimator( dist, h )
+@time fft = fit( fft, x, bits=20 );
 
 @time ffty = pdf.( fft, r );
 plot!( p, r, ffty, label="", size=[1000,800] )
