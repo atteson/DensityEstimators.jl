55c05639090e09aa1588675c4be6a48d90cabe63
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 13:40:45 +0200

    Silverman interpolation

--- src/DensityEstimators.jl
+++ src/DensityEstimators.jl
@@ -1,9 +1,11 @@
+
 module DensityEstimators
 
 using Random
 using Distributions
 using StatsBase
 using DSP
+using Interpolations
 
 export BruteForceKernelDensityEstimator, FFTKernelDensityEstimator
 
@@ -31,14 +33,16 @@ struct FFTKernelDensityEstimator{T} <: AbstractKernelDensityEstimator{T}
     dist::T
     h::Float64
     data::Vector{Float64}
-    centers::LinRange{Float64,Int64}
-    estimates::Vector{Float64}
+    interpolator::AbstractInterpolation{Float64,1}
 end
 
 FFTKernelDensityEstimator( dist::T, h::Float64 ) where {T} =
-    FFTKernelDensityEstimator( dist, h, Float64[], LinRange( 0.0, 0.0, 0 ), Float64[] );
+    FFTKernelDensityEstimator(
+        dist, h, Float64[], LinearInterpolation( Float64[], Float64[] )
+    );
 
-function StatsBase.fit( estimator::FFTKernelDensityEstimator{T}, data::AbstractVector{Float64};
+function StatsBase.fit( estimator::FFTKernelDensityEstimator{T},
+                        data::AbstractVector{Float64};
                         bits = 14,
                         ) where {T}
     dist = estimator.dist
@@ -48,11 +52,29 @@ function StatsBase.fit( estimator::FFTKernelDensityEstimator{T}, data::AbstractV
     (m,M) = extrema(data)
     N = 2^bits
     M += eps(M) # histograms are left closed
-    range = LinRange( m, M, N+1 )
+    range = LinRange( m, M, N )
     centers = (range[1:end-1] + range[2:end])/2
     hist = fit( Histogram, data, range )
-    @assert( sum(hist.weights) == n )
-    w = hist.weights./n;
+
+    interpolator = LinearInterpolation( range, 1:length(range) )
+    parts = divrem.( interpolator.( data ), 1 )
+    int = Int.(getindex.(parts,1))
+    frac = getindex.(parts,2)
+    nfrac = 1 .- frac
+    test = getindex.( [range], int ) .* nfrac
+    test .+= get.( [range], int .+ 1, range[end] ) .* frac
+    @assert( maximum(abs.(test .- data)) .< 1e-8 )
+    
+    weights = zeros( length(range) )
+    for i = 1:length(int)
+        weights[int[i]] += nfrac[i]
+        if int[i] < length(weights)
+            weights[int[i]+1] += frac[i]
+        end
+    end
+    
+    @assert( abs(sum(weights) - n) < 1e-8 )
+    weights ./= n;
 
     # choose width of convolution kernel; assume symmetry
     dx = (range.stop - range.start)/range.len
@@ -63,22 +85,15 @@ function StatsBase.fit( estimator::FFTKernelDensityEstimator{T}, data::AbstractV
     centers2 = (range2[1:end-1] + range2[2:end])/2
     d = pdf.( dist, centers2./h )./h;
     
-    estimates = conv( d, w )[factor:factor+N-1];
+    estimates = conv( d, weights )[factor:factor+N-1];
     @assert(!any(estimates.<0))
 
-    return FFTKernelDensityEstimator( dist, h, data, centers, estimates )
+    return FFTKernelDensityEstimator(
+        dist, h, data, LinearInterpolation( range, estimates )
+    )
 end
 
-function Distributions.pdf( kde::FFTKernelDensityEstimator{T}, x::Float64 ) where {T}
-    centers = kde.centers
-    estimates = kde.estimates
-    r = searchsorted( centers, x )
-    if( r.start == 0 || r.stop > length(centers) )
-        error( "Out of range!" )
-    end
-    lambda = (x - centers[r.stop])/(centers[r.start] - centers[r.stop])
-    return estimates[r.stop] + lambda * (estimates[r.start] - estimates[r.stop])
-end
+Distributions.pdf( kde::FFTKernelDensityEstimator{T}, x::Float64 ) where {T} = kde.interpolator( x )
 
 function Random.rand!( rng::AbstractRNG, kde::AbstractKernelDensityEstimator{T}, v::AbstractArray{Float64} ) where {T}
     for i = 1:length(v)
--- test/test.jl
+++ test/test.jl
@@ -12,23 +12,34 @@ N = 100
 Random.seed!(1)
 x = rand( dist, N );
 
+s=[1000,800]
+s=[600,500]
 @time bf = fit( bf, x );
 r = -2:0.001:2
-p = histogram( x, normalize=:pdf, fillalpha=0.0, size=[1000,800] )
+p = histogram( x, normalize=:pdf, fillalpha=0.0, size=s )
 @time bfy = pdf.( bf, r );
 plot!( p, r, bfy, label="" )
 
 fft = FFTKernelDensityEstimator( dist, h )
-@time fft = fit( fft, x, bits=20 );
-
+@time fft = fit( fft, x );
 @time ffty = pdf.( fft, r );
-plot!( p, r, ffty, label="", size=[1000,800] )
+plot!( p, r, ffty, label="" )
 
-maximum(abs.(bfy - ffty))
-mean(abs.(bfy - ffty))
-sqrt(mean((bfy - ffty).^2))
+bits = 10:20
+accuracies = fill( NaN, 3, length(bits) )
+for i=1:length(bits)
+    @time fft = fit( fft, x, bits=bits[i] );
+    @time ffty = pdf.( fft, r );
+
+    accuracies[1,i] = mean(abs.(bfy - ffty))
+    accuracies[2,i] = sqrt(mean((bfy - ffty).^2))
+    accuracies[3,i] = maximum(abs.(bfy - ffty))
+end
 
 emp = ecdf( x )
-p = plot( r, emp.(r), label="", size=[1000,800] )
+p = plot( r, emp.(r), label="", size=s )
 @time y = cdf.( bf, r )
 plot!( p, r, y, label="" )
+
+
+
