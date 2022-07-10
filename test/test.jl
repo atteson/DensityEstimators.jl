e9605cbb0fe4092d12a56db798ae53abc4fd52e2
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 06:16:55 +0200

    test of skinny kernel accuracy

--- test/test.jl
+++ test/test.jl
@@ -25,6 +25,8 @@ fft = FFTKernelDensityEstimator( dist, h )
 @time ffty = pdf.( fft, r );
 plot!( p, r, ffty, label="" )
 
+sqrt(mean((bfy .- ffty).^2))
+
 bits = 10:20
 accuracies = fill( NaN, 3, length(bits) )
 for i=1:length(bits)
@@ -48,3 +50,38 @@ Random.seed!(1)
 @assert( !any((x .> 1/3) .& (x .< 2/3)) );
 @assert( !any((x .> 1/9) .& (x .< 2/9)) );
 @assert( !any((x .> 7/9) .& (x .< 8/9)) );
+
+N = 100
+Random.seed!(1)
+@time x = rand( c, N );
+@time bf = fit( bf, x );
+r = 0.001:0.001:0.999
+@time bfy = pdf.( bf, r );
+
+@time fft = fit( fft, x );
+@time ffty = pdf.( fft, r );
+
+sqrt(mean((bfy .- ffty).^2))
+
+N = 100
+Random.seed!(1)
+x = rand( dist, N );
+r = -2:0.001:2
+
+hs = 10 .^ -1:6
+accuracies = fill( NaN, 3, length(hs) )
+for i = 1:length(hs)
+    bf = BruteForceKernelDensityEstimator( dist, hs[i] )
+    @time bf = fit( bf, x );
+    @time bfy = pdf.( bf, r );
+
+    fft = FFTKernelDensityEstimator( dist, hs[i] )
+    @time fft = fit( fft, x )
+    @time ffty = pdf.( fft, r );
+
+    accuracies[1,i] = mean(abs.(bfy - ffty))
+    accuracies[2,i] = sqrt(mean((bfy - ffty).^2))
+    accuracies[3,i] = maximum(abs.(bfy - ffty))
+end
+
+accuracies[2,:]
